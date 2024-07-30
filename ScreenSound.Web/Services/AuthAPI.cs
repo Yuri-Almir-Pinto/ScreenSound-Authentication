8ac1e425fc6f6f2848b6ff37c4287c891bff5df6
Yuri-Almir-Pinto <yurialmirp@gmail.com>
2024-07-30 12:55:32 -0300

    Atualizar AuthAPI
    Consertado a classe "AuthAPI" para corretamente determinar quando o usuário está logado ou não, sem lançar uma exceção.
    
    Adicionado também o DTO "InfoPessoaResponse".

--- ScreenSound.Web/Services/AuthAPI.cs
+++ ScreenSound.Web/Services/AuthAPI.cs
@@ -1,11 +1,36 @@
-﻿using ScreenSound.Web.Response;
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using ScreenSound.Web.Response;
 using System.Net.Http.Json;
+using System.Security.Claims;
 
 namespace ScreenSound.Web.Services;
 
-public class AuthAPI(IHttpClientFactory factory)
+public class AuthAPI(IHttpClientFactory factory) : AuthenticationStateProvider
 {
     private readonly HttpClient _httpClient = factory.CreateClient("API");
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var pessoa = new ClaimsPrincipal();
+
+        var response = await _httpClient.GetAsync("auth/manage/info");
+
+        if (response.IsSuccessStatusCode)
+        {
+            var info = await response.Content.ReadFromJsonAsync<InfoPessoaResponse>();
+
+            List<Claim> dados = 
+                [
+                    new Claim(ClaimTypes.Name, info.Email),
+                    new Claim(ClaimTypes.Name, info.Email)
+                ];
+
+            var identity = new ClaimsIdentity(dados, "Cookies");
+            pessoa = new ClaimsPrincipal(identity);
+        }
+
+        return new AuthenticationState(pessoa);
+    }
 
     public async Task<AuthResponse> LoginAsync(string email, string senha)
     {
@@ -16,7 +41,10 @@ public class AuthAPI(IHttpClientFactory factory)
         });
 
         if (response.IsSuccessStatusCode)
+        {
+            NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
             return new AuthResponse { Success = true };
+        }
         else
             return new AuthResponse { Success = false, Errors = ["Login ou senha inválidos."] };
     }
